ca0ac7d516ffb4bbe4dc876d1a8e11a54c60bfdd
mattaugustt <122750956+mattaugustt@users.noreply.github.com>
2023-03-29 15:49:35 -0300

    adicionando as repostas obtidas.
    1. Google -> 4.
    2. Americanas -> 1.
    3. Número de empresas = 2.
    4. Número de empresas = 3.

--- consultas.sql
+++ consultas.sql
@@ -1,6 +1,5 @@
 -- Responda as seguintes perguntas
--- 1. Qual empresa utiliza o maior número de tecnologias na última pesquisa
-(2/2022)?
+-- 1. Qual empresa utiliza o maior número de tecnologias na última pesquisa (2/2022)?
 
 SELECT empresa.nome, COUNT(*) as num_tecnologias
 FROM empresa
@@ -9,6 +8,10 @@ WHERE relatorio.data_compilacao between '2022-07-01' and '2022-12-31'
 GROUP BY empresa.nome
 ORDER BY num_tecnologias DESC
 LIMIT 1;
+
+-- Resposta obtida: Google -> 4.
+
+
 
 -- 2. Qual empresa utilizava o menor número de tecnologias na pesquisa anterior (1/2022)?
 
@@ -20,6 +23,10 @@ GROUP BY empresa.nome
 ORDER BY num_tecnologias ASC
 LIMIT 1;
 
+-- Resposta obtida: Americanas -> 1.
+
+
+
 -- 3. Quantas empresas utilizam tecnologias da área de “Dados” atualmente?
 
 SELECT COUNT(DISTINCT empresa_id) as num_empresas
@@ -30,6 +37,10 @@ WHERE tecnologia_id IN (
   WHERE descricao LIKE '%Dados%'
 );
 
+-- Resposta obtida: número de empresas = 2.
+
+
+
 -- 4. Quantas empresas utilizam tecnologias que não são da área de “Dados” atualmente?
 
 SELECT COUNT(DISTINCT empresa_id) as num_empresas
@@ -39,3 +50,5 @@ WHERE tecnologia_id NOT IN (
   FROM tecnologia
   WHERE descricao LIKE '%Dados%'
 );
+
+-- Resposta obtida: número de empresas = 3.
