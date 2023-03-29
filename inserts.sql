49d0cbf678740720e89e1e90eaf2a6983fc1342e
mattaugustt <122750956+mattaugustt@users.noreply.github.com>
2023-03-29 11:49:02 -0300

    Arquivo que vai popular o banco de dados.
    COM DADOS FICTÍCIOS!

---
+++ inserts.sql
@@ -0,0 +1,52 @@
+-- Insert empresas:
+INSERT INTO empresa (id_empresa, nome, endereco, cnpj, telefone, email) VALUES
+(1, 'Americanas', 'Av. das Flores, 2590', '111.222.345/098', '(21)98888-9999', 'americanasa@gmail.com'),
+(2, 'Samsung', 'Av. Nova York, 2235', '029.984.748/738', '(41)93747-3647', 'samsungsa@gmail.com'),
+(3, 'Google', 'Av. Presidente Vargas, 2345', '122.344.546/896', '(31)95647-2364', 'googlesa@gmail.com'),
+(4, 'Amazon', 'Av. Duque Leopoldo, 4647', '455.655.776/844', '(11)96474-3744', 'amazonsa@gmail.com');
+
+
+
+-- Insert tecnologias:
+INSERT INTO tecnologia (id_tec, nome, descricao) VALUES
+(1, 'Excel', 'Dados em tabelas'),
+(2, 'PowerBi', 'Análise de dados'),
+(3, 'Tableau', 'Análise de dados'),
+(4, 'Google EarthEngine', 'Análise de dados espaciais'),
+(5, 'Pyhton', 'Programação'),
+(6, 'SQL', 'Banco de dados'),
+(7, 'C++', 'Programação'),
+(8, 'JavaScript', 'Programação'),
+(9, 'Machine Learning', 'Aprendizado de maquina'),
+(10, 'Amazon AWS', 'Dados em nuvem'),
+(11, 'React Native', 'Criação de Aplicativos');
+
+
+
+-- Insert relatório (primeiro semestre):
+INSERT INTO relatorio (id_relatorio, data_compilacao, empresa_id, tecnologia_id) VALUES
+(1, '2022-01-12', 3, 8),
+(2, '2022-02-22', 1, 2),
+(3, '2022-03-18', 4, 10),
+(4, '2022-01-10', 3, 4),
+(5, '2022-05-19', 2, 8),
+(6, '2022-06-25', 3, 5),
+(7, '2022-04-07', 3, 9),
+(8, '2022-03-19', 2, 11),
+(9, '2022-04-24', 4, 6),
+(10, '2022-02-27', 4, 5);
+
+
+
+-- Insert relatório (segundo semestre):
+INSERT INTO relatorio (id_relatorio, data_compilacao, empresa_id, tecnologia_id) VALUES
+(11, '2022-07-13', 3, 4),
+(12, '2022-08-10', 1, 2),
+(13, '2022-08-19', 3, 5),
+(14, '2022-09-29', 2, 11),
+(15, '2022-10-27', 3, 9),
+(16, '2022-11-29', 1, 6),
+(17, '2022-11-22', 3, 11),
+(18, '2022-12-14', 2, 5),
+(19, '2022-12-02', 4, 5),
+(20, '2022-09-29', 4, 10);
